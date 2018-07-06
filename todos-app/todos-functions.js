4622ef9a6a7a7bf4b8f3b4a9860e3a9e434ecb27
Gino Diaz <gino.diaz@uhc.com>
2018-07-06 10:29:32 -0700

    refactor it again

--- todos-app/todos-functions.js
+++ todos-app/todos-functions.js
@@ -15,3 +15,66 @@ const resetTodoInput = function () {
     document.querySelector('#edit-done').checked = false;
     document.querySelector('#submit-todo').innerHTML = 'Add Todo';
 }
+
+const renderTodos = function (todos, filters) {
+    let filterTodos = todos.filter(function (todo) {
+        /*         if (filters.searchText === ''){
+                    return todos;
+                }else{ */
+        return todo.todo.toLowerCase().includes(filters.searchText.toLowerCase());
+        //}
+    });
+
+    filterTodos = filterTodos.filter(function (todo) {
+        return !filters.hideCompleted || !todo.isDone;
+    });
+
+    document.querySelector('#todo-list').innerHTML = '';
+    if (filterTodos.length === 1) {
+        replaceTodo.wasText = filterTodos[0].todo  //todo.todo;        
+        document.querySelector('#focus-todo').value = filterTodos[0].todo  //todo.todo;        
+        document.querySelector('#edit-done').checked = filterTodos[0].isDone;
+        document.querySelector('#submit-todo').innerHTML = 'Update Todo';
+    } else {
+        resetTodoInput();
+    }
+    filterTodos.forEach(function (todo) {
+        const todoEl = document.createElement('p');
+        todoEl.textContent = todo.todo.concat((todo.isDone) ? ' *' : '');
+        /*if (filters.hideCompleted){
+              if (!todo.isDone)
+                todoEl.textContent = todo.todo
+          } else {
+              todoEl.textContent = todo.todo.concat((todo.isDone) ? ' *' : '');
+          } */
+        document.querySelector('#todo-list').appendChild(todoEl);
+    })
+    document.querySelector('#count-todos').innerHTML = `There ${(filterTodos.length > 1) ? 'are' : 'is'} ${filterTodos.length} ${(filterTodos.length > 1) ? 'todos' : 'todo'}.`
+} 
+
+const editReplaceTodo = function (todos, replaceTodo) {
+
+    const toReplaceIdx = todos.findIndex(function (todo) {
+        return todo.todo.toLowerCase() === replaceTodo.wasText.toLowerCase()
+    });
+    todos[toReplaceIdx].todo = replaceTodo.replaceText;
+    todos[toReplaceIdx].isDone = replaceTodo.isDone;
+    document.querySelector('#todo-list').innerHTML = '';
+    document.querySelector('#search-todo').value = '';
+    filters.searchText = '';
+
+    renderTodos(todos, filters);
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+const addReplaceTodo = function (todos, replaceTodo) {
+    const newTodo = { todo: replaceTodo.replaceText, isDone: replaceTodo.isDone }
+    todos.push(newTodo);
+    filters.searchText = '';
+    document.querySelector('#focus-todo').value = '';
+    renderTodos(todos, filters);
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+}
